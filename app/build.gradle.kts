ccccb7376e31582596c1c26c4a1eb9353af17594
tgyuuAn <atg0614@gmail.com>
2025-06-05 20:55:26 +0900

    1.0.7 -> 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     namespace = "com.tgyuu.ebbingplanner"
 
     defaultConfig {
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
         targetSdk = 35
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
