ecdad9f79a5562a2da0ded5b168e70ee6d0ddd51
tgyuuAn <atg0614@gmail.com>
2025-06-30 17:55:28 +0900

    1.0.13 -> 1.0.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     namespace = "com.tgyuu.ebbingplanner"
 
     defaultConfig {
-        versionCode = 14
-        versionName = "1.0.13"
+        versionCode = 15
+        versionName = "1.0.14"
         targetSdk = 35
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
