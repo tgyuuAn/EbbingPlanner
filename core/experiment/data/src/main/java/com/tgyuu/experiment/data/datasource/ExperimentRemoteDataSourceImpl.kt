d44014337558f0464a2d7e9091dea17c4ea8d5c4
tgyuuAn <atg0614@gmail.com>
2026-05-24 17:55:15 +0900

    fix : 실험군 variant 대소문자 불일치로 항상 CONTROL 반환되던 버그 수정

--- core/experiment/data/src/main/java/com/tgyuu/experiment/data/datasource/ExperimentRemoteDataSourceImpl.kt
+++ core/experiment/data/src/main/java/com/tgyuu/experiment/data/datasource/ExperimentRemoteDataSourceImpl.kt
@@ -1,6 +1,5 @@
 package com.tgyuu.experiment.data.datasource
 
-import android.util.Log
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.tgyuu.experiment.data.BuildConfig
 import com.tgyuu.experiment.data.datasource.ExperimentRemoteDataSource
@@ -21,14 +20,12 @@ class ExperimentRemoteDataSourceImpl @Inject constructor(
                     experimentKeys.forEach { key ->
                         val configKey = getConfigKey(key)
                         val value = remoteConfig.getString(configKey)
-                        Log.d("test", "key : $key value : $value")
 
                         if (value.isNotEmpty()) {
                             result[key] = value
                         }
                     }
                 }
-
                 continuation.resume(result)
             }
         }
